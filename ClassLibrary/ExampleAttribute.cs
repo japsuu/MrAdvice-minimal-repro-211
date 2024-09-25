0c37febc1894a14342f916fb40bc3445d0b95b1c
Jasper <japsu.honkasalo@gmail.com>
2024-09-25 08:44:12 +0300

    Add minimal repro example

---
+++ ClassLibrary/ExampleAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using ArxOne.MrAdvice.Advice;
+
+namespace ClassLibrary1;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class ExampleAttribute : Attribute, IMethodAdvice
+{
+    public void Advise(MethodAdviceContext context)
+    {
+        // Remove the following line, and the project compiles successfully
+        const BindingFlags flags = BindingFlags.Instance | BindingFlags.NonPublic;
+
+        context.Proceed();
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibrary1;
+
+namespace Test1;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        MethodWithAttribute();
+    }
+    
+    
+    [Example]
+    private static void MethodWithAttribute()
+    {
+        Console.WriteLine("test");
+    }
+}
